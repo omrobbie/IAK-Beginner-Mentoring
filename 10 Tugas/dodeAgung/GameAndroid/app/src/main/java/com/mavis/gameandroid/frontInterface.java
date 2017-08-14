343b65b1ed81aeac83138a0a3f9515026032bcf9
omrobbie <putu.roby@yahoo.com>
2017-08-14 16:27:47 +0800

    clean up

---
+++ 10 Tugas/dodeAgung/GameAndroid/app/src/main/java/com/mavis/gameandroid/frontInterface.java
@@ -0,0 +1,93 @@
+package com.mavis.gameandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class frontInterface extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_interface);
+        final Button button3A = (Button) findViewById(R.id.btn3A);
+        final Button button2A = (Button) findViewById(R.id.btn2A);
+        final Button buttonA = (Button) findViewById(R.id.btnA);
+        final Button button3B = (Button) findViewById(R.id.btn3B);
+        final Button button2B = (Button) findViewById(R.id.btn2B);
+        final Button buttonB = (Button) findViewById(R.id.btnB);
+        final Button buttonUndo = (Button) findViewById(R.id.btnUndo);
+        final TextView txtscoreA = (TextView) findViewById(R.id.scoreA);
+        final TextView txtscoreB = (TextView) findViewById(R.id.scoreB);
+
+
+        button3A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int scoreTemp = (int) Integer.parseInt(txtscoreA.getText().toString());
+                scoreTemp += 3;
+                txtscoreA.setText(String.valueOf(scoreTemp));
+            }
+        });
+        button2A.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int scoreTemp = (int) Integer.parseInt(txtscoreA.getText().toString());
+                scoreTemp += 2;
+                txtscoreA.setText(String.valueOf(scoreTemp));
+            }
+        });
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int scoreTemp = (int) Integer.parseInt(txtscoreA.getText().toString());
+                scoreTemp += 1;
+                txtscoreA.setText(String.valueOf(scoreTemp));
+            }
+        });
+        buttonUndo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtscoreA.setText("0");
+                txtscoreB.setText("0");
+            }
+        });
+        button3B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int scoreTemp = (int) Integer.parseInt(txtscoreB.getText().toString());
+                scoreTemp += 3;
+                txtscoreB.setText(String.valueOf(scoreTemp));
+            }
+        });
+        button2B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int scoreTemp = (int) Integer.parseInt(txtscoreB.getText().toString());
+                scoreTemp += 2;
+                txtscoreB.setText(String.valueOf(scoreTemp));
+            }
+        });
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int scoreTemp = (int) Integer.parseInt(txtscoreB.getText().toString());
+                scoreTemp += 1;
+                txtscoreB.setText(String.valueOf(scoreTemp));
+            }
+        });
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        final TextView txtscoreA = (TextView) findViewById(R.id.scoreA);
+        final TextView txtscoreB = (TextView) findViewById(R.id.scoreB);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putString("textscoreA",txtscoreA.getText().toString());
+        savedInstanceState.putString("textscoreB",txtscoreB.getText().toString());
+    }
+}
